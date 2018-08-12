4343c002877953b9ffc6f174ecd6a8ef614fa5f2
vonlyinno <v.only.inno@qq.com>
2018-08-12 15:05:28 +0800

    修改 css

--- pages/mail/mail.js
+++ pages/mail/mail.js
@@ -2,7 +2,7 @@
  * @Author: tinniehe 
  * @Date: 2018-08-11 20:08:50 
  * @Last Modified by: tinniehe
- * @Last Modified time: 2018-08-11 20:32:05
+ * @Last Modified time: 2018-08-12 13:57:16
  */
 
 import { $wuxToast } from '../../components/index'
@@ -24,10 +24,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   async onLoad (options) {
-    if(!options.mail_id) {
-      wx.navigateBack()
-      return
-    }
+    // if(!options.mail_id) {
+    //   wx.navigateBack()
+    //   return
+    // }
     if(options.type === 'mail') {
       //only mail
       this.setData({
@@ -39,14 +39,56 @@ Page({
       url: GET__MAIL_URL,
       data: {
         data: {
-          mail_id: options.mail_id
+          //mail_id: options.mail_id
         }
       }
     }
-    let mail = await this.requestApi(opt)
+    //let mail = await this.requestApi(opt)
+    let mail = {
+      "errno": 0,
+      "data": {
+          "user_id": "1",
+          "mail_id": "1",
+          "templet_id": 1,
+          "poster_id": 1,
+          "poster_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534016080759&di=5578d267bb43ad31cfe8ad0419024eac&imgtype=0&src=http%3A%2F%2Fimg5.duitang.com%2Fuploads%2Fitem%2F201411%2F02%2F20141102115809_FavJH.thumb.700_0.jpeg",
+          "friend_name": "何娇羞",
+          "friend_addr": "",
+          "friend_email": "",
+          "create_time": "2018.08.06",
+          "arrived_time": "2018.08.22",
+          "content":[{
+            type: "text",
+            text: "在写这篇文章之前，我询问了在唯品会和腾讯的童鞋、以及公司里面前端大神（深哥），对于设计稿切图的详细方法，经过对比验证，得出设计稿转换页面单位尺寸方法步骤。我分别询问下面四个问题："
+          },{
+            type: "pic",
+            url: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534015977245&di=2f862303a3adeb50bc27fb65503bae93&imgtype=0&src=http%3A%2F%2Fcdnq.duitang.com%2Fuploads%2Fitem%2F201504%2F08%2F20150408H1237_L42Ym.jpeg"
+          }, {
+            type: "text",
+            text: "在写这篇文章之前，我询问了在唯品会和腾讯的童鞋、以及公司里面前端大神（深哥），对于设计稿切图的详细方法，经过对比验证，得出设计稿转换页面单位尺寸方法步骤。我分别询问下面四个问题："
+          }, {
+            type: "pic",
+            url: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1534015977245&di=2f862303a3adeb50bc27fb65503bae93&imgtype=0&src=http%3A%2F%2Fcdnq.duitang.com%2Fuploads%2Fitem%2F201504%2F08%2F20150408H1237_L42Ym.jpeg"
+          }],
+          "time_line":[{
+            "poster_status": "信使在北京迷路啦",
+            "mood_time": "2018.08.03",
+            "mood": ''
+          },{
+            "poster_status": "信使在北京迷路啦",
+            "mood_time": "2018.08.05",
+            "mood": '夜跑减肥，睡觉前进行夜跑，尽量保证30分钟以上，多喝水，可以吃少量低热量的食物。运动时会消耗大量血液和肝脏内的糖原，在睡觉时身体会自我调整体内平衡，燃烧脂肪补足血液和肝糖，从而达到减脂的效果。部分朋友跑步后很兴奋，以至于失眠睡不着，建议别太晚跑。 ​​​'
+          },],
+          "is_read": ""
+      }
+    }
     if(mail.errno === 0) {
+      let mood = mail.data.time_line.filter((item) => {
+        return item.mood !== ''
+      })
+      mail.data.mood_count = mood.length
       this.setData({
-        mail
+        mail: mail.data
       })
     }
   },
